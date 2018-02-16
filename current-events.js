fb6b601d4b39c6a80f94321cc9ff69fb1a6f2f30
annaholubecki2018 <36522819+annaholubecki2018@users.noreply.github.com>
2018-02-16 10:40:43 -0600

    Update current-events.js

--- current-events.js
+++ current-events.js
@@ -1,10 +1,3 @@
-let n = 0;
-let firstname = [];
-let lastname = [];
-let email = [];
-let username = [];
-let password = [];
-
 $(document).ready(function() {
 	$(".interest-tag-wrapper").click(function() {
 		if ($(this).find('img').css('opacity') == '0.4') {
@@ -16,15 +9,3 @@ $(document).ready(function() {
 	});
 
 });
-
-function NewUser() {
-	document.getElementById("done1").onclick = function(){
-  //get the value in each text box and add to user arrays
-  firstname[n] = document.getElementById("first-name-field").value;
-	lastname[n] = document.getElementById("last-name-field").value;
-	email[n] = document.getElementById("email-field").value;
-	username[n] = document.getElementById("username-field").value;
-	password[n] = document.getElementById("password-field").value;
-	n = n + 1
-}
-}
