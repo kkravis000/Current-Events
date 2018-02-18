9f4d6544376e09f889dc9bb829017d237755f4d4
Shiyu Luo <34426871+shiyuLuo2019@users.noreply.github.com>
2018-02-17 23:29:40 -0600

    modified choose-interest.html & current-events.css
    1. modified choose-interest.html
    2. change class names, styles & page layout in current-events.css

--- current-events.js
+++ current-events.js
@@ -1,3 +1,14 @@
+
+/*
+Global variables.
+*/
+
+let firstname = [];
+let lastname = [];
+let email = [];
+let username = [];
+let password = [];
+
 $(document).ready(function() {
 	$(".interest-tag-wrapper").click(function() {
 		if ($(this).find('img').css('opacity') == '0.4') {
@@ -7,10 +18,27 @@ $(document).ready(function() {
 			$(this).find('img').css('opacity', '0.4');
 		}
 	});
+
+	$("#sign-up-done").click(function() {
+		var n = firstname.length;
+		firstname[n] = $("#first-name-field").val();
+		lastname[n] = $("#last-name-field").val();
+		email[n] = $("#email-field").val();
+		password[n] = $("#password-field").val();
+	});
+
 });
 
-$(document).ready(function(){
-    $("#submit-sign-up").click(function(){
-        $(".sign-up-confirmation").show();
-    });
-});
+/**
+function NewUser() {
+	document.getElementById("done1").onclick = function(){
+	//get the value in each text box and add to user arrays
+	firstname[n] = document.getElementById("first-name-field").val();
+	lastname[n] = document.getElementById("last-name-field").val();
+	email[n] = document.getElementById("email-field").val();
+	username[n] = document.getElementById("username-field").val();
+	password[n] = document.getElementById("password-field").val();
+}
+}
+
+**/
