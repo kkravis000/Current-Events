9867ab789b46d9ae2033a982f208f801a6a5b526
kkravis000 <keithkravis2018@u.northwestern.edu>
2018-02-18 19:26:09 -0600

    Fixed links and fixed the settings dropdown menu

---
+++ sign-up.js
@@ -0,0 +1,12 @@
+var 
+
+
+function validateForm()
+{
+	var FirstNameField = document.getElementById("first-name-field");
+	var LastNameField = document.getElementById("last-name-field");
+	var EmailField = document.getElementById("e-mail-field");
+	var UsernameField = document.getElementById("username-field");
+	var PasswordField = document.getElementById("password-field");
+	var ConfirmPasswordField = document.getElementById("confirm-password-field");	
+}
