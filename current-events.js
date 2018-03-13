9bacabd2ceec309d59091f427efc176663a433c0
Shiyu Luo <34426871+shiyuLuo2019@users.noreply.github.com>
2018-03-13 00:49:07 -0500

    Add files via upload

--- current-events.js
+++ current-events.js
@@ -42,14 +42,18 @@ var articleBackgroundsSeasonal = [
 "linear-gradient(to bottom,rgba(255, 255, 255, 0.3) 100%,rgba(255, 255, 255, 0.3) 100%), url(\"flu.jpg\")",
 ];
 
-var featuredArticleLinks = [
-"https://www.cnn.com/2018/02/17/politics/white-house-responds-mueller/index.html",
-"http://abcnews.go.com/International/75-magnitude-earthquake-rocks-mexico/story?id=53154547"
-];
-
 var featuredArticleTitles = [
 "Trump responds to Mueller indictment news",
-"7.2 magnitude earthquake rocks Mexico, 2 killed in chopper crash"
+"Florida lawmakers face political pressure after Parkland shooting",
+"Meghan Markle and Prince Harry's romantic date night gets political",
+"Mueller files new fraud charges against Paul Manafort and Rick Gates"
+];
+
+var featuredBackgrounds = [
+"linear-gradient(to bottom,rgba(255, 255, 255, 0.3) 100%,rgba(255, 255, 255, 0.3) 100%), url(\"trump.jpg\")",
+"linear-gradient(to bottom,rgba(255, 255, 255, 0.3) 100%,rgba(255, 255, 255, 0.3) 100%), url(\"florida.jpg\")",
+"linear-gradient(to bottom,rgba(255, 255, 255, 0.3) 100%,rgba(255, 255, 255, 0.3) 100%), url(\"meghan.jpg\")",
+"linear-gradient(to bottom,rgba(255, 255, 255, 0.3) 100%,rgba(255, 255, 255, 0.3) 100%), url(\"mueller.jpg\")",
 ];
 
 
@@ -60,7 +64,7 @@ var featuredArticle = 0;
 
 var numPoliticalArticles = 4;
 var numSeasonalArticles = 4;
-var numFeaturedArticles = 2;
+var numFeaturedArticles = featuredArticleTitles.length;
 
 $(document).ready(function() {
 	$(".interest-tag-wrapper").click(function() {
@@ -163,7 +167,7 @@ $(document).ready(function() {
 
 function SignUpConfirmation() {
 		var x = document.getElementById("SignUpConfirmation");
-    var a = document.forms["create-account-fields"]["first-name-field"].value;
+    	var a = document.forms["create-account-fields"]["first-name-field"].value;
 		var b = document.forms["create-account-fields"]["last-name-field"].value;
 		var d = document.forms["create-account-fields"]["e-mail-field"].value;
 		var e = document.forms["create-account-fields"]["username-field"].value;
@@ -196,36 +200,41 @@ function SignUpConfirmation() {
 }
 
 function init() {
-	document.getElementById("event-title-one").innerHTML = articleTitlesPolitical[politicalArticle].link(articleLinksPolitical[politicalArticle]);
-	document.getElementById("event-title-two").innerHTML = articleTitlesSeasonal[seasonalArticle].link(articleLinksSeasonal[seasonalArticle]);
+	document.getElementById("event-title-one").innerHTML = articleTitlesPolitical[politicalArticle];
+	document.getElementById("event-politics").setAttribute("href", articleLinksPolitical[politicalArticle]);
+	document.getElementById("event-title-two").innerHTML = articleTitlesSeasonal[seasonalArticle];
+	document.getElementById("event-seasonal").setAttribute("href", articleLinksSeasonal[seasonalArticle]);
 }
-
 
 function nextArticleFirst() 
 {
 	politicalArticle = (politicalArticle + 1) % numPoliticalArticles;
-	document.getElementById("event-title-one").innerHTML = articleTitlesPolitical[politicalArticle].link(articleLinksPolitical[politicalArticle]);
+	document.getElementById("event-politics").setAttribute("href", articleLinksPolitical[politicalArticle]);
+	document.getElementById("event-title-one").innerHTML = articleTitlesPolitical[politicalArticle];
 	document.getElementById("event-politics").style.backgroundImage = articleBackgroundsPolitical[politicalArticle];
 }
 
 function prevArticleFirst() 
 {
 	politicalArticle = (politicalArticle + numPoliticalArticles - 1) % numPoliticalArticles;
-	document.getElementById("event-title-one").innerHTML = articleTitlesPolitical[politicalArticle].link(articleLinksPolitical[politicalArticle]);
+	document.getElementById("event-politics").setAttribute("href", articleLinksPolitical[politicalArticle]);
+	document.getElementById("event-title-one").innerHTML = articleTitlesPolitical[politicalArticle];
 	document.getElementById("event-politics").style.backgroundImage = articleBackgroundsPolitical[politicalArticle];
 }
 
 function nextArticleSecond()
 {
 	seasonalArticle = (seasonalArticle + 1) % numSeasonalArticles;
-	document.getElementById("event-title-two").innerHTML = articleTitlesSeasonal[seasonalArticle].link(articleLinksSeasonal[seasonalArticle]);
+	document.getElementById("event-title-two").innerHTML = articleTitlesSeasonal[seasonalArticle];
+	document.getElementById("event-seasonal").setAttribute("href", articleLinksSeasonal[seasonalArticle]);
 	document.getElementById("event-seasonal").style.backgroundImage = articleBackgroundsSeasonal[seasonalArticle];
 }
 
 function prevArticleSecond()
 {
 	seasonalArticle = (seasonalArticle + numSeasonalArticles - 1) % numSeasonalArticles;
-	document.getElementById("event-title-two").innerHTML = articleTitlesSeasonal[seasonalArticle].link(articleLinksSeasonal[seasonalArticle]);
+	document.getElementById("event-title-two").innerHTML = articleTitlesSeasonal[seasonalArticle];
+	document.getElementById("event-seasonal").setAttribute("href", articleLinksSeasonal[seasonalArticle]);
 	document.getElementById("event-seasonal").style.backgroundImage = articleBackgroundsSeasonal[seasonalArticle];
 }
 
@@ -233,13 +242,15 @@ function prevArticleSecond()
 function nextArticleFeatured()
 {
 	featuredArticle = (featuredArticle + 1) % numFeaturedArticles;
-	document.getElementById("featured-event-title").innerHTML = featuredArticleTitles[featuredArticle].link(featuredArticleLinks[featuredArticle]);
+	document.getElementById("featured-event-title").innerHTML = featuredArticleTitles[featuredArticle].link("under-construction.html");
+	document.getElementById("event-featured").style.backgroundImage = featuredBackgrounds[featuredArticle];
 }
 
 function prevArticleFeatured()
 {
 	featuredArticle = (featuredArticle + 1) % numFeaturedArticles;
-	document.getElementById("featured-event-title").innerHTML = featuredArticleTitles[featuredArticle].link(featuredArticleLinks[featuredArticle]);
+	document.getElementById("featured-event-title").innerHTML = featuredArticleTitles[featuredArticle].link("under-construction.html");
+	document.getElementById("event-featured").style.backgroundImage = featuredBackgrounds[featuredArticle];
 }
 
 init();
